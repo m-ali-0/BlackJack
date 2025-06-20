e8c3cad28838e4329f67692618f6ecca849b2648
m-ali-0 <amehdizada16118@ada.edu.az>
2025-06-20 11:42:29 +0400

    upgraded display, fixed errors and logic

--- BJ.py
+++ BJ.py
@@ -1,4 +1,5 @@
-import secrets
+import secrets, time
+delay = .4
 
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -23,7 +24,48 @@ class Deck:
             
     def deal_card(self):
         return self.cards.pop()
-    
+class Player:
+    def __init__(self, name, deck):
+        self.name = name
+        self.hand = [deck.deal_card(), deck.deal_card()]
+        
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def value(self):
+        return hand_value(self.hand)
+
+    def ToString(self):
+        res = ''
+        for card in self.hand:
+            res += f'{call[card[0]]} of {card[1]}'
+            if card != self.hand[-1]:
+                res += ', '
+        return res
+
+    def show_hand(self, reveal_all=True):
+        label = "Your" if self.name.lower() == "player" else f"{self.name}'s"
+        if not reveal_all:
+            return f"{label} hand: {call[self.hand[0][0]]} of {self.hand[0][1]}, ()?()"
+        return f"{label} hand: {self.ToString()} (value: {self.value()})"
+
+    def has_blackjack(self):
+        return self.value() == 21 and len(self.hand) == 2
+
+    def is_busted(self):
+        return self.value() > 21
+
+
+def type_text(text, delay=0.05,newline = True):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    if newline:
+        print()
+
+def sl(n):
+    time.sleep(n)
+       
 def hand_value(hand):
     value = sum(values[card[0]] for card in hand)
     aces = sum(1 for card in hand if card[0] == 'A')
@@ -32,74 +74,116 @@ def hand_value(hand):
         aces -= 1
     return value
 
-def handToString(hand):
-    res = ''
-    for card in hand:
-        res += f'{call[card[0]]} of {card[1]}'
-        if card != hand[-1]:
-            res += ', '
-    return res
-    
-
-def print_hand(hand, who = 'Dealer'):
-    if who == 'P':
-        print(f"Your hand: {handToString(hand)} (value: {hand_value(hand)})")
-    else:
-        print(f"Dealer's hand: {handToString(hand)} (value: {hand_value(hand)})")
-    
+ 
 def get_answer(forw):
     if forw == 'hs':
-        return input("Hit or Stand? ").lower()
+        type_text("Hit or Stand? ", newline = False)
+        return input().lower()
     if forw == 'continue':
-        return True if input("Wanna play more?(yes or no): ")=='yes' else False 
+        type_text("Wanna play more?(yes or no): ", newline=False)
+        return True if input() in {'yes','sure','why not','yes please','yepp'} else False 
+
+def reveal(player, dealer):
+    if player.has_blackjack() and dealer.has_blackjack():
+        type_text(player.show_hand())
+        type_text(dealer.show_hand())
+        type_text("Both have Blackjack. It's a tie!")
+        return True
+    elif player.has_blackjack():
+        type_text(player.show_hand())
+        type_text(dealer.show_hand())
+        type_text("Blackjack! You win!")
+        return True
+    elif dealer.has_blackjack():
+        type_text(player.show_hand())
+        type_text(dealer.show_hand())
+        type_text("Dealer has Blackjack. You lose.")
+        return True
+    return False
 
 
 def play_blackjack():
     deck = Deck()
-    player = [deck.deal_card(), deck.deal_card()]
-    dealer = [deck.deal_card(), deck.deal_card()]
+    # player = [deck.deal_card(), deck.deal_card()]
+    # dealer = [deck.deal_card(), deck.deal_card()]
+    
+    player = Player("Player",deck)
+    dealer = Player('Dealer',deck)
+    
+    if reveal(player,dealer):
+        return      
 
-    print_hand(player, "P") #######
-    print(f"Dealer's hand: {call[dealer[0][0]]} of {dealer[0][1]}, ()?()")
-
-    while hand_value(player) < 21:
+    type_text(player.show_hand())
+    type_text(dealer.show_hand(reveal_all=False))
+    
+    # type_text(handToStr(player, "P")) #######
+    # type_text(f"Dealer's hand: {call[dealer[0][0]]} of {dealer[0][1]}, ()?()")
+    
+    while player.value() < 21:
         move = get_answer('hs')
-        if move == 'hit':
-            player.append(deck.deal_card())
-            print_hand(player, "P")
+        if move in {'hit', 'h','yes','sure','yepp'}:
+            player.add_card(deck.deal_card())
+            type_text(player.show_hand())
         else:
             break
 
-    if hand_value(player) > 21:
-        print("Bust! You lose.")
+    if player.value() > 21:
+        type_text("Bust! You lose.")
         return
 
-    print_hand(dealer)
-    while hand_value(dealer) < 17:
-        dealer.append(deck.deal_card())
-        print_hand(dealer)
+    type_text(dealer.show_hand())
+    while dealer.value() < 17:
+        dealer.add_card(deck.deal_card())
+        type_text(dealer.show_hand())
 
-    pv = hand_value(player)
-    dv = hand_value(dealer)
+    pv = player.value()
+    dv = dealer.value()
 
     if dv > 21 or pv > dv:
-        print("You win!")
+        type_text("You win!")
     elif pv < dv:
-        print("Dealer wins.")
+        type_text("Dealer wins.")
     else:
-        print("It's a tie!")
+        type_text("It's a tie!")
 
-cont = True
-print('-------------------------------------')
-print('Hello! Wanna lose some money ha? xaxa\nWhy not? Go ahead!\nAnd don\'t forget to enjoy!')
-print('-------------------------------------')
-print('\n'*1)
 
-while cont:
-    play_blackjack()
-    cont = get_answer("continue")
 
-print('\n'*1)
-print('-------------------------------------')
-print('Thanks for playing! Hope you enjoyed!\nAnd see you later, alligator!')
-print('-------------------------------------')
+def printer(txt):
+    if txt == 'hi':
+        sl(delay)
+        type_text('-------------------------------------')
+        type_text('Hello! Wanna lose some money ha? xaxa')
+        sl(delay)
+        type_text('Why not? Go ahead!')
+        sl(delay)
+        type_text('And don\'t forget to enjoy!')
+        sl(delay)
+        type_text('-------------------------------------')
+        sl(delay)
+        print('\n')
+    else:
+        print('\n'*1)
+        type_text('-------------------------------------')
+        sl(delay)
+        type_text('Thanks for playing! Hope you enjoyed!')
+        sl(delay)
+        type_text('And see you later, alligator!')
+        sl(delay)
+        type_text('-------------------------------------')
+        sl(delay)
+
+
+def main():
+    cont = True
+    printer('hi')
+    while cont:
+        play_blackjack()
+        cont = get_answer("continue")
+        sl(delay)
+    printer('bye')
+    
+try:
+    main()
+except KeyboardInterrupt as err:
+    type_text('Program stopped by user. Thanks for playing!')
+    exit()
